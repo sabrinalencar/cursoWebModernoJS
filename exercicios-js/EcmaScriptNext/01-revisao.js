24aa1f83dc4e9e47aae36b58406062a467d8d237
sabrinalencar <sabrina@ejcm.com.br>
2020-06-19 20:16:17 -0300

    +ES revisão

---
+++ exercicios-js/EcmaScriptNext/01-revisao.js
@@ -0,0 +1,28 @@
+// ES06 - 2015  ES07 - 2016  ES08 - 2017
+
+// let e const 
+{
+    var a = 2
+    let b = 3
+    console.log(b) // let tem escopo de bloco só é visível aqui
+}
+console.log(a)// var não tem escopo de bloco
+
+// Template String
+const produto = 'iPad'
+console.log(`${produto} é caro`)
+console.log(`${produto} 
+é 
+caro`)
+
+// Destructuring + operador rest (rest params)
+const [l, e, t, ...ras] = "string"
+console.log(l, e, t, ras) // s t r ['i', 'n', 'g']
+
+const [x, y] = [1, 2]
+console.log(x, y)
+const [, b, c] = [1, 2, 3]
+console.log(b, c)
+
+const { idade: i, nome: name } = { nome: 'Sabrina', idade: '20' }
+console.log(i, name)
